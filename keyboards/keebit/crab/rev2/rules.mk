608d728cd86bcc496fd0963eca2e446f7155d328
numToStr <vikasraj1911@gmail.com>
2023-08-06 14:32:34 +0530

    promicro and arcadia

---
+++ keyboards/keebit/crab/rev2/rules.mk
@@ -0,0 +1,20 @@
+# # arcadia pinout
+#
+# GP10             GP11
+#
+# D3:GP0           RAW:RAW
+# D2:GP1           GND:GND
+# GND:GND          RST:RST
+# GND:GND          VCC:3.3V
+# D1:GP2           F4:GP29
+# D0:GP3           F5:GP28
+# D4:GP4           F6:GP27
+# C6:GP5           F7:GP26
+# D7:GP6           B1:GP22
+# E6:GP7           B3:GP20
+# B4:GP8           B2:GP23
+# B5:GP9           B6:GP21
+#
+# GP12 GP13 GP14 GP15 GP16
+
+SERIAL_DRIVER = vendor
